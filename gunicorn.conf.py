5178dc134bc1865191299aec0e85907b590b1b5b
Antishake-Cell <antishake605@gmail.com>
2025-12-08 14:54:11 +0530

    Update default deployment port to 3000

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ import multiprocessing
 import os
 
 # Server socket
-bind = f"0.0.0.0:{os.getenv('PORT', '8001')}"
+bind = f"0.0.0.0:{os.getenv('PORT', '3000')}"
 backlog = 2048
 
 # Worker processes
